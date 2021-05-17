4622121175243dd35cca6e8f1ecafb90093b8610
Ahmed Mohamed <62860063+ahmedMetwaly@users.noreply.github.com>
2021-05-18 01:36:44 +0200

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -14,7 +14,7 @@ const socket = io();
 
 
 
-const videoElement = document.getElementsByClassName('input_video');
+const videoElement = document.getElementsByClassName('input_video')[0];
 const canvasElement = document.getElementsByClassName('output_canvas')[0];
 const canvasCtx = canvasElement.getContext('2d');
 
