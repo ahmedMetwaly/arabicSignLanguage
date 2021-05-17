666476a882c916a342771d76e8dcb228d46c5a6d
Ahmed Mohamed <62860063+ahmedMetwaly@users.noreply.github.com>
2021-05-17 23:59:05 +0200

    Add files via upload

---
+++ signsToApp.py
@@ -0,0 +1,167 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+import pickle
+from flask_socketio import SocketIO, emit
+import numpy as np
+from flask import Flask, render_template
+import pandas as pd
+from playsound import playsound
+from gtts import gTTS
+import os
+
+
+# In[2]:
+
+
+
+app = Flask(__name__, static_url_path='')
+app.config['SECRET_KEY'] = 'somethingsoamazingomg!!!'
+socketio = SocketIO(app)
+
+
+
+def converter(sign):
+    if sign=='wahed':
+        return 'واحد'
+    elif sign=='etnyn':
+        return 'أثنين' 
+    elif sign=='tlata':
+        return 'ثلاثة' 
+    elif sign=='arb3a':
+        return 'أربعة'                
+    elif sign=='5msa':
+        return 'خمسة' 
+    elif sign=='sta':
+        return 'ستة' 
+    elif sign=='sb3a':
+        return 'سبعة' 
+    elif sign=='tmnya':
+        return 'ثمانية' 
+    elif sign=='ts3a':
+        return 'تسعة' 
+    elif sign=='34ra':
+        return 'عشرة' 
+    elif sign=='ahln':
+        return 'اهلا' 
+    elif sign=='esmk':
+        return 'اسمك'     
+    elif sign=='eyh':
+        return 'ايه' 
+    elif sign=='el3nwan':
+        return 'العنوان'
+    elif sign=='nt3rf':
+        return 'نتعرف'      
+    elif sign=='3aml':
+        return 'عامل' 
+    elif sign=='kwys':
+        return 'كويس' 
+    else : return 'لا استطيع فهمك'     
+
+
+
+def most_frequent(List):
+    counter = 0
+    num = List[0]
+      
+    for i in List:
+        curr_frequency = List.count(i)
+        if(curr_frequency> counter):
+            counter = curr_frequency
+            num = i
+  
+    return num
+
+
+def className():
+    ClassName = most_frequent(mostAppearence)
+    mostAppearence.clear()
+    SpeakSign(converter(ClassName))
+    print(ClassName)    
+
+    
+
+
+
+with open('signs4.pkl', 'rb') as f:
+    model = pickle.load(f)
+
+
+# In[3]:
+
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@socketio.on('talk')
+def SpeakSign(sign):
+    mySign = gTTS(text=sign, lang='ar', slow=False)
+    mySign.save("sign.mp3")
+    playsound("sign.mp3")
+    os.remove("sign.mp3")
+
+
+
+@socketio.on('play')
+def Play(audio):
+    playsound(audio)
+
+
+# In[4]:
+
+mostAppearence = []
+counter =0 
+
+@socketio.on('upload') 
+def predict(landmarks):
+    global counter
+    #pose coordinates
+    pose = landmarks['_pose']
+    pose_row = list(np.array(
+        [ [landmark['x'], landmark['y'], landmark['z'], landmark['visibility'] ] for landmark in pose]).flatten())
+    
+    #left hand coordinates
+    left_hand = landmarks['_leftHand']
+    left_hand_row = list(np.array(
+        [ [landmark['x'], landmark['y'], landmark['z'], 0 ] for landmark in left_hand]).flatten())
+   
+
+    # right hand coordinates
+    right_hand = landmarks['_rightHand']
+    right_hand_row = list(np.array(
+        [ [landmark['x'], landmark['y'], landmark['z'], 0 ] for landmark in right_hand]).flatten())
+
+    rowRLH = pose_row+right_hand_row+left_hand_row
+    # Make Detections
+    X = pd.DataFrame([rowRLH])
+    body_language_class = model.predict(X)[0]
+    mostAppearence.append(body_language_class)
+    #body_language_prob = model.predict_proba(X)[0]
+    print(body_language_class)
+    counter=counter+1
+    if counter==30:
+        counter=0
+        className()
+    
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.config["DEBUG"] = False
+    socketio.run(app,ssl_context="adhoc")
+
+
+# In[ ]:
+
+
+
+
---
+++ static/js/app.js
@@ -0,0 +1,114 @@
+// socketio initialization
+//<script type="module">
+
+
+var Pcounter = 0;
+var Lcounter = 0;
+var Rcounter = 0;
+var Stop =false; 
+
+
+const socket = io();
+
+
+
+
+
+const videoElement = document.getElementsByClassName('input_video')[0];
+const canvasElement = document.getElementsByClassName('output_canvas')[0];
+const canvasCtx = canvasElement.getContext('2d');
+
+function onResults(results) {
+  canvasCtx.save();
+  canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+  canvasCtx.drawImage(
+      results.image, 0, 0, canvasElement.width, canvasElement.height);
+  drawConnectors(canvasCtx, results.poseLandmarks, POSE_CONNECTIONS,
+                {color: '#00FF00', lineWidth: 4});
+  drawLandmarks(canvasCtx, results.poseLandmarks,
+                {color: '#FF0000', lineWidth: 2});
+  drawConnectors(canvasCtx, results.faceLandmarks, FACEMESH_TESSELATION,
+                {color: '#C0C0C070', lineWidth: 1});
+  drawConnectors(canvasCtx, results.leftHandLandmarks, HAND_CONNECTIONS,
+                {color: '#CC0000', lineWidth: 5});
+  drawLandmarks(canvasCtx, results.leftHandLandmarks,
+                {color: '#00FF00', lineWidth: 2});
+  drawConnectors(canvasCtx, results.rightHandLandmarks, HAND_CONNECTIONS,
+                {color: '#00CC00', lineWidth: 5});
+  drawLandmarks(canvasCtx, results.rightHandLandmarks,
+                {color: '#FF0000', lineWidth: 2});
+              
+  
+  //if(results.leftHandLandmarks==undefined){
+    //  Lcounter++;
+      //if(Lcounter==30){
+        //socket.emit('play','moveBack.mp3')
+        //Lcounter=0;
+        //Stop = false
+      //}
+      //console.log("left hand is Not intialized");
+  //}
+
+  if(results.rightHandLandmarks==undefined || results.leftHandLandmarks==undefined || results.poseLandmarks==undefined){
+      Rcounter++;
+      if(Rcounter==90){
+        socket.emit('play','moveBack.mp3')
+        Rcounter=0
+        Stop = false
+
+      }
+    console.log("right hand is Not intialized");
+
+  }
+  //if(results.poseLandmarks==undefined){
+    // Pcounter++;
+      //if(Pcounter==60){
+        //socket.emit('play','moveBack.mp3')
+        //Pcounter=0
+        //Stop = false
+      //}
+
+    //console.log("pose is Not intialized");
+  //}
+
+  if(results.poseLandmarks!=undefined && results.rightHandLandmarks!=undefined && results.leftHandLandmarks!=undefined){
+    Pcounter++;
+    if(Pcounter==30){
+      socket.emit('play','start.mp3')
+    }
+      
+    
+    var landmarks =   {'_pose': results.poseLandmarks,
+      '_leftHand':results.leftHandLandmarks,
+      '_rightHand':results.rightHandLandmarks}
+    console.log('before prediction')
+    socket.emit('upload',landmarks)   
+    console.log('after prediction')
+ 
+  }
+
+  canvasCtx.restore();
+}
+
+
+const holistic = new Holistic({locateFile: (file) => {
+  return `https://cdn.jsdelivr.net/npm/@mediapipe/holistic/${file}`;
+}});
+holistic.setOptions({
+  modelComplexity: 1,
+  smoothLandmarks: true,
+  minDetectionConfidence: 0.5,
+  minTrackingConfidence: 0.5
+});
+holistic.onResults(onResults);
+const camera = new Camera(videoElement, {
+  onFrame: async () => {
+    await holistic.send({image: videoElement});
+  },
+  width: 1280,
+  height: 720
+});
+camera.start();
+
+
+
