69d4d1c6854dd3fda417d0b822333d554cce93aa
Ahmed Mohamed <62860063+ahmedMetwaly@users.noreply.github.com>
2021-05-19 07:13:35 +0200

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -24,11 +24,11 @@ socket.on('voice', function(voice){
   audio.play(); //
 })
 
-socket.on('sleep', function(ms){
-  return new Promise(
-    resolve => setTimeout(resolve, ms)
-  );
-})
+//socket.on('sleep', function(ms){
+//  return new Promise(
+//    resolve => setTimeout(resolve, ms)
+//  );
+//})
 
 
 function onResults(results) {
