d514caf01fa99b61be8f81192e63c0fdd3704c23
Ahmed Mohamed <62860063+ahmedMetwaly@users.noreply.github.com>
2021-05-19 22:05:26 +0200

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -19,10 +19,17 @@ const canvasElement = document.getElementsByClassName('output_canvas')[0];
 const canvasCtx = canvasElement.getContext('2d');
 
 
-socket.on('voice', function(voice){
-  var audio = document.getElementById("audio").setAttribute('src',voice );
-  audio.play(); //
+function voice(vc){
+  var audio = document.getElementById("audio").setAttribute('src',vc );
+  audio.play(); 
+}
+
+
+socket.on('voice', function(vc){
+  voice(vc)
 })
+
+
 
 
 
@@ -51,7 +58,7 @@ function onResults(results) {
   if(results.rightHandLandmarks==undefined || results.leftHandLandmarks==undefined || results.poseLandmarks==undefined){
       Rcounter++;
       if(Rcounter==90){
-        emit('voice','https://firebasestorage.googleapis.com/v0/b/sign-s-voices.appspot.com/o/%D9%8A%D8%AF%D9%83%20%D8%BA%D9%8A%D8%B1%20%D9%88%D8%A7%D8%B6%D8%AD%D9%87.mp3?alt=media&token=50709ce5-c3aa-4cf0-bb1f-23bd2ee1dfef')
+        voice('https://firebasestorage.googleapis.com/v0/b/sign-s-voices.appspot.com/o/%D9%8A%D8%AF%D9%83%20%D8%BA%D9%8A%D8%B1%20%D9%88%D8%A7%D8%B6%D8%AD%D9%87.mp3?alt=media&token=50709ce5-c3aa-4cf0-bb1f-23bd2ee1dfef')
         socket.emit('sleep',6)
         console.log('call after sleep')
         Rcounter=0
@@ -65,7 +72,7 @@ function onResults(results) {
   if(results.poseLandmarks!=undefined && results.rightHandLandmarks!=undefined && results.leftHandLandmarks!=undefined){
     Pcounter++;
     if(Pcounter==30){
-      emit('voice','https://firebasestorage.googleapis.com/v0/b/sign-s-voices.appspot.com/o/%D8%A7%D8%A8%D8%AF%D8%A3%D8%B9%D9%85%D9%84%20%D8%A7%D9%84%D8%A7%D8%B4%D8%A7%D8%B1%D8%A9.mp3?alt=media&token=e1636125-6106-40f9-9a1c-c82344e2945f')
+      voice('https://firebasestorage.googleapis.com/v0/b/sign-s-voices.appspot.com/o/%D8%A7%D8%A8%D8%AF%D8%A3%D8%B9%D9%85%D9%84%20%D8%A7%D9%84%D8%A7%D8%B4%D8%A7%D8%B1%D8%A9.mp3?alt=media&token=e1636125-6106-40f9-9a1c-c82344e2945f')
       socket.emit('sleep',3)
       console.log('call after sleep and all done')
       Pcounter=0
