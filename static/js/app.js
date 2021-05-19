c536b978acce4bd04a9a385a7f29b222c781394c
Ahmed Mohamed <62860063+ahmedMetwaly@users.noreply.github.com>
2021-05-19 21:29:20 +0200

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -47,20 +47,11 @@ function onResults(results) {
                 {color: '#FF0000', lineWidth: 2});
               
   
-  //if(results.leftHandLandmarks==undefined){
-    //  Lcounter++;
-      //if(Lcounter==30){
-        //socket.emit('play','moveBack.mp3')
-        //Lcounter=0;
-        //Stop = false
-      //}
-      //console.log("left hand is Not intialized");
-  //}
-
+ 
   if(results.rightHandLandmarks==undefined || results.leftHandLandmarks==undefined || results.poseLandmarks==undefined){
       Rcounter++;
       if(Rcounter==90){
-        socket.emit('voice','https://firebasestorage.googleapis.com/v0/b/sign-s-voices.appspot.com/o/%D9%8A%D8%AF%D9%83%20%D8%BA%D9%8A%D8%B1%20%D9%88%D8%A7%D8%B6%D8%AD%D9%87.mp3?alt=media&token=50709ce5-c3aa-4cf0-bb1f-23bd2ee1dfef')
+        emit('voice','https://firebasestorage.googleapis.com/v0/b/sign-s-voices.appspot.com/o/%D9%8A%D8%AF%D9%83%20%D8%BA%D9%8A%D8%B1%20%D9%88%D8%A7%D8%B6%D8%AD%D9%87.mp3?alt=media&token=50709ce5-c3aa-4cf0-bb1f-23bd2ee1dfef')
         socket.emit('sleep',6)
         console.log('call after sleep')
         Rcounter=0
@@ -74,7 +65,7 @@ function onResults(results) {
   if(results.poseLandmarks!=undefined && results.rightHandLandmarks!=undefined && results.leftHandLandmarks!=undefined){
     Pcounter++;
     if(Pcounter==30){
-      socket.emit('voice','https://firebasestorage.googleapis.com/v0/b/sign-s-voices.appspot.com/o/%D8%A7%D8%A8%D8%AF%D8%A3%D8%B9%D9%85%D9%84%20%D8%A7%D9%84%D8%A7%D8%B4%D8%A7%D8%B1%D8%A9.mp3?alt=media&token=e1636125-6106-40f9-9a1c-c82344e2945f')
+      emit('voice','https://firebasestorage.googleapis.com/v0/b/sign-s-voices.appspot.com/o/%D8%A7%D8%A8%D8%AF%D8%A3%D8%B9%D9%85%D9%84%20%D8%A7%D9%84%D8%A7%D8%B4%D8%A7%D8%B1%D8%A9.mp3?alt=media&token=e1636125-6106-40f9-9a1c-c82344e2945f')
       socket.emit('sleep',3)
       console.log('call after sleep and all done')
       Pcounter=0
